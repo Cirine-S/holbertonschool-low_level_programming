8c4ed5e202a4c28c0bd51effe77b3167e5be5560
Cirine92 <1410@holbertonschool.com>
2020-02-06 23:02:44 +0000

    l

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
 * main - Entry point
-*
+* advanced task
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-nt i, j;
+int i, j;
 for (i = 0; i < 9; i++)
 {
 for (j = i + 1; j < 10; j++)

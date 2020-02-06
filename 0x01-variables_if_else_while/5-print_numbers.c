b1c2e876321577f91e605d045fe4e00d7ebbe481
Cirine92 <1410@holbertonschool.com>
2020-02-06 22:00:07 +0000

    g

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* single digit numbers
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+printf("%i", i);
+putchar ('\n');
+return (0);
+}

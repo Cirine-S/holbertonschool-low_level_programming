ee626041a593d49b791ffa87b17198b71c999ca2
Cirine92 <1410@holbertonschool.com>
2020-02-06 22:59:16 +0000

    j

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+nt i, j;
+for (i = 0; i < 9; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (i < 8)
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}

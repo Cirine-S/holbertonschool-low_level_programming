d9a6dbfd3160884fce9bb1a0f5457093546cf2fa
Cirine92 <1410@holbertonschool.com>
2020-02-11 13:06:43 +0000

    ugg

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_alphabet - function
+ **/
+void print_alphabet_x10(void)
+{
+  char ch;
+  int i;
+  for (i = 1; i <= 10; i++)
+    {  for (ch = 'a'; ch <= 'z'; ch++)
+    {
+      _putchar(ch);
+    }
+  _putchar('\n');
+    }
+}

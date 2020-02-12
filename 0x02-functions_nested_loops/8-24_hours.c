3ce4a3c13a718c459d5bd48f41e09ab3aeb36518
Cirine92 <1410@holbertonschool.com>
2020-02-12 08:52:05 +0000

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,15 +5,16 @@
  */
 void jack_bauer(void)
 {
+int i, j;
 for (i = 00; i <= 23; i++)
 {
 for (j = 00; j <= 59; j++)
 {
-_putchar(i / 10);
-_putchar(i % 10);
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
 _putchar(':');
-_putchar(j / 10);
-_putchar(j % 10);
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
 _putchar('\n');
 }
 }

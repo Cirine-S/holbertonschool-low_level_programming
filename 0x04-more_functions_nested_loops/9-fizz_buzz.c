4b67760192fc2d6c688fd3453d5d7af9ed08fef1
Cirine92 <1410@holbertonschool.com>
2020-02-13 15:24:05 +0000

    m

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,25 +10,15 @@ int i;
 for (i = 1; i < 101; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
-{
 printf("FizzBuzz");
-printf(" ");
-}
 else if (i % 3 == 0)
-{
 printf("Fizz");
-printf(" ");
-}
 else if (i % 5 == 0)
-{
 printf("Buzz");
-printf(" ");
-}
 else
-{
 printf("%d", i);
+if (i < 100)  
 printf(" ");
-}
 }
 printf("\n");
 return (0);

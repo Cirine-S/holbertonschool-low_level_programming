cebd5018ee6ce586eeaa238bd950d48fa2366ffe
Cirine92 <1410@holbertonschool.com>
2020-02-16 19:44:45 +0000

    even fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int fib, f1 = 1, f2 = 2, sum = 0;
+while (f2 < 4000000)
+{
+if (f2 % 2 == 0)  
+sum = sum + f2;
+fib = f1 + f2;
+f1 = f2;
+f2 = fib;  
+}
+printf("%d", sum);
+printf("\n");
+return (0);
+}

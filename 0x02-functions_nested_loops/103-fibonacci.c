c24dc42682c830d46e3617563e47365afbd8f5d8
Cirine92 <1410@holbertonschool.com>
2020-02-16 19:47:02 +0000

    even fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,11 +8,11 @@ int main(void)
 int fib, f1 = 1, f2 = 2, sum = 0;
 while (f2 < 4000000)
 {
-if (f2 % 2 == 0)  
+if (f2 % 2 == 0)
 sum = sum + f2;
 fib = f1 + f2;
 f1 = f2;
-f2 = fib;  
+f2 = fib;
 }
 printf("%d", sum);
 printf("\n");

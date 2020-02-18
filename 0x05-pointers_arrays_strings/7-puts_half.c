8c2929d03a70a829bbdeb8856fbd27c418d458b9
Cirine92 <1410@holbertonschool.com>
2020-02-18 19:23:07 +0000

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,28 +5,28 @@
  */
 void puts_half(char *str)
 {
-	int length_of_the_string = 0, i = 0;
+int length_of_the_string = 0, i = 0;
 
-	while (str[length_of_the_string] != '\0')
-	{
-		length_of_the_string++;
-	}
-	if (length_of_the_string % 2 == 0)
-	{
-                i = length_of_the_string / 2;
-		while (i < length_of_the_string)
-		{
-			_putchar(str[i]);
-			i++;
-		}
-	}
-		else
-		{      i = (length_of_the_string / 2) + 1;
-			while (i < length_of_the_string)
-			{
-				_putchar(str[i]);
-				i++;
-			}
-		}
-	_putchar('\n');
+while (str[length_of_the_string] != '\0')
+{
+length_of_the_string++;
+}
+if (length_of_the_string % 2 == 0)
+{
+i = length_of_the_string / 2;
+while (i < length_of_the_string)
+{
+_putchar(str[i]);
+i++;
+}
+}
+else
+{      i = (length_of_the_string / 2) + 1;
+while (i < length_of_the_string)
+{
+_putchar(str[i]);
+i++;
+}
+}
+_putchar('\n');
 }

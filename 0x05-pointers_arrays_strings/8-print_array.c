73346f12e5b0f0b078c92b93079bba1b260e1611
Cirine92 <1410@holbertonschool.com>
2020-02-18 21:14:52 +0000

    trying to correct a fail output

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,8 +7,11 @@
 void print_array(int *a, int n)
 {
 int i;
-for (i = 0; i < n - 1; i++)
-printf("%d, ", a[i]);
-
+for (i = 0; i < n; i++)
+{
+if (i == n - 1)
 printf("%d\n", a[n - 1]);
+else
+printf("%d, ", a[i]);
+}
 }

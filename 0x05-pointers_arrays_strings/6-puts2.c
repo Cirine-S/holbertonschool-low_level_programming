62d653a615086ba03a6e95c4a21b37c108a8966d
Cirine92 <1410@holbertonschool.com>
2020-02-18 13:25:35 +0000

    fixing loop

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,10 +6,11 @@
 void puts2(char *str)
 {
 int i = 0;
-while (str[i] != '\0')
+do
 {
 _putchar(str[i]);
-i = i + 2;
+i +=2 ;
 }
+while ((str[i] != '\0') && (str[i - 1] != '\0'));
 _putchar('\n');
 }

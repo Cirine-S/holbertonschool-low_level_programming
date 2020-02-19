472ef05322a9b6880d6cacd8f27a4d0241bff2cd
Cirine92 <1410@holbertonschool.com>
2020-02-19 13:26:36 +0000

    advanced

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ *_atoi - function
+ *@s: char
+ *Return: 0
+ */
+int _atoi(char *s)
+{
+int i = 0, j = 0, n = 0;
+while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+{
+if (s[i] == '-')
+j++;
+i++;
+}
+
+while (s[i] != '\0')
+{
+if (s[i] >= '0' && s[i] <= '9')
+n = n * 10 + (s[i] - '0');
+if (s[i] < '0' || s[i] > '9')
+break;
+i++;
+}
+
+if (j % 2 != 0)
+n = n * -1;
+
+return (n);
+}

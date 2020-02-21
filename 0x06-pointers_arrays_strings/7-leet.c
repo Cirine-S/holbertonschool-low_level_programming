1bcff548f1adf09bd1e7e13dbcba3ddcfb667fe7
Cirine92 <1410@holbertonschool.com>
2020-02-21 13:09:25 +0000

    correct a variable

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,14 +8,14 @@
 char *leet(char *a)
 {
 int  i, j;
-char l[] = "oOlLeEaAtT";
-char n[] = "0011334477";
+char c[] = "aAeEoOtTlL";
+char r[] = "4433007711";
 while (a[i] != '\0')
 {
-for (j = 0; l[j] != '\0'; j++)
+for (j = 0; c[j] != '\0'; j++)
 {
-if (a[i] == l[j])
-a[i] = n[j];
+if (a[i] == c[j])
+a[i] = r[j];
 }
 i++;
 }

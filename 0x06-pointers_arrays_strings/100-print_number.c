5da61f83ecec86523f7ffe18229489fb150e65ba
Cirine92 <1410@holbertonschool.com>
2020-02-21 16:21:19 +0000

    print numbers

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * print_number - function
+ *@n: int
+ * Return: Null
+ */
+void print_number(int n)
+{
+int i, a, x;
+if (n < 0)
+{
+_putchar('-');
+n = n * -1;
+}
+if (n >= 10)
+{
+for (i = 1000000000; i >= 10; i = i / 10)
+{
+x = n / i;
+if (x != 0)
+{
+_putchar(x + '0');
+while (i >= 10)
+{
+a = ((n % i) / (i / 10));
+_putchar(a + '0');
+i = i / 10;
+}
+}
+}
+}
+else
+_putchar(n + '0');
+}

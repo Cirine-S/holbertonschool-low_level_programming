4092ee80063f94a602edc39b66b90650ef36134f
Cirine92 <1410@holbertonschool.com>
2020-02-21 10:32:50 +0000

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,16 +9,10 @@
 int _strcmp(char *s1, char *s2)
 {
 int i, j;
-
+j = 0;
 for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 {
 if (s1[i] != s2[i])
-{
 j = s1[i] - s2[i];
-s1[i + 1] = '\0';
-}
-else
-j = 0;
-}
 return (j);
 }

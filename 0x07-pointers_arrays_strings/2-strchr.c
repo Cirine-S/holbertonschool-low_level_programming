7d885827c076ae2fcfe96b524f266683fb0616e9
Cirine92 <1410@holbertonschool.com>
2020-02-24 13:08:52 +0000

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,14 @@
 char *_strchr(char *s, char c)
 {
 int i = 0;
-
+char *p;
 while (s[i] != '\0' &&  s[i] != c)
 i++;
 if (s[i] == '\0')
-	return (0);
+return (0);
 else
 {
-return (&s[i]);
+p = &s[i];
+return (p);
 }
 }

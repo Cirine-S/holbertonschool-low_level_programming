5e5d498018ca867e1ab2359402c47277c8580312
Cirine92 <1410@holbertonschool.com>
2020-02-24 19:11:57 +0000

    4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ *_strpbrk - function
+ *@s: char
+ *@accept: char
+ *Return: char
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+int i, j;
+for (i = 0; s[i] != '\0'; i++)
+{
+j = 0;
+while (accept[j] != '\0')
+{
+if (accept[j] == s[i])
+break;
+else
+j++;
+}
+if (accept[j] == s[i])
+break;
+}
+return (&s[i]);
+}

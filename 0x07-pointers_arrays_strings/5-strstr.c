1b4bbfc25b85926e27770686640dc7c6f3a14fb7
Cirine92 <1410@holbertonschool.com>
2020-02-24 23:26:15 +0000

    5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,10 +7,15 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-int i;
+int i, j;
 for (i = 0; haystack[i] != '\0'; i++)
 {
 if (haystack[i] == needle[0])
+{
+while (needle[j] != '\0' && needle[j] == haystack[i+j])
+j++;
+}
+if (needle[j] == '\0')
 return (&needle[0]);
 }
 return (0);

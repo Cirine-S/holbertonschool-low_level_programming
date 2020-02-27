2901dcc8341411fcc68111f7c8e0a4bc0c7917d0
Cirine92 <1410@holbertonschool.com>
2020-02-27 22:59:48 +0000

    advanced

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -14,11 +14,12 @@ int wildcmp(char *s1, char *s2)
 	if (*s2 == '*')
 		return (wildcmp(s1, s2 + 1));
 
+	if (*s1 == *s2)
+                return (wildcmp(s1 + 1, s2 + 1));
+
 	if ((*s2 != '*' && *s2 != '\0') && *s1)
 		return (wildcmp(s1 + 1, s2));
 
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
 
 return (0);
 }

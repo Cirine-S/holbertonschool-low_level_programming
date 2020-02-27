20c03337f8cb126bfa7520a15239ebe676611485
Cirine92 <1410@holbertonschool.com>
2020-02-27 22:57:32 +0000

    advanced

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -11,13 +11,13 @@ int wildcmp(char *s1, char *s2)
 	if ((*s1 == '\0' && *s2 == '\0') || (*s2 == '*' && *(s2 + 1) == '\0'))
 		return (1);
 
-	else if (*s2 == '*')
+	if (*s2 == '*')
 		return (wildcmp(s1, s2 + 1));
 
-	else if ((*s2 != '*' && *s2 != '\0') && *s1)
+	if ((*s2 != '*' && *s2 != '\0') && *s1)
 		return (wildcmp(s1 + 1, s2));
 
-	else if (*s1 == *s2)
+	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 
 return (0);
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -8,3 +8,4 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int _putchar(char c);
 void _endofstring (char *s);
+int wildcmp(char *s1, char *s2);

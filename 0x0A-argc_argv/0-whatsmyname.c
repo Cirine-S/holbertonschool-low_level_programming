ed0605e22a79cdca8f45cd25f9cc7b8fe09ca1ef
Cirine92 <1410@holbertonschool.com>
2020-03-01 21:03:24 +0000

    printing prog name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function
+ *@argc: number of arguments
+ *@argv: array of strings
+ *Return: 0
+ */
+int main (int argc, char *argv[])
+{
+	argc = 0;
+       printf ("The name of this program is \"%s\".\n",argv[argc]);
+       return (0);
+}

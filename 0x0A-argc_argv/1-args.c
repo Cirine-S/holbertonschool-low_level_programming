2a66030440183c0ba72b2c5c6ca60cce09ded1c8
Cirine92 <1410@holbertonschool.com>
2020-03-01 21:24:24 +0000

    printing argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function
+ *@argc: number of arguments
+ *@argv: array of strings
+ *Return: 0
+ */
+int main(int argc, char **argv)
+{
+printf("%d\n", argc - 1);
+argv ++;
+return (0);
+}

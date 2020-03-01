2cfc3632f32b987d3f64d4c012c6a60d7ed63fd2
Cirine92 <1410@holbertonschool.com>
2020-03-01 22:52:34 +0000

    change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: integer
+ * @argv: char pointer
+ * Return: int 0 or 1
+ */
+int main(int argc, char const *argv[])
+{
+	int n;
+
+	if (argc != 2)
+	{
+		puts("Error");
+		return (1);
+	}
+	n = atoi(argv[1]);
+	if (n <= 0)
+	{
+		puts("0");
+		return (1);
+	}
+	return (0);
+}

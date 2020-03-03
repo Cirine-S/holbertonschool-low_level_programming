fb0ef42ed9a9212eb9b877aa578dbb227e3681b2
Cirine92 <1410@holbertonschool.com>
2020-03-03 22:53:05 +0000

    1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,7 +16,7 @@ char *_strdup(char *str)
 		return (NULL);
 	for (len = 0; str[len] != '\0'; len++)
 		;
-	s = malloc((len + 1) * sizeof(char));
+	s = malloc(len + 1);
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; i < len; i++)

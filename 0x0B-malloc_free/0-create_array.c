06259463b62b78f428525d156d7a470d9c2e3fae
Cirine92 <1410@holbertonschool.com>
2020-03-03 21:38:09 +0000

    array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,17 +5,16 @@
  *create_array - function
  *@size: int
  *@c: char
- *Return: char
+ *Return: array
  */
 
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *a;
-	c = 'H';
 	a = malloc(size *sizeof(char));
 	if (size == 0 || a == NULL)
-		return NULL;
+		return ("null");
 	for (i = 0; i < size; i++)
 		a[i] = c;
 	return (a);

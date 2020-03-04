825d2c3795bd737f6cec4536f8c78e730d915e06
Cirine92 <1410@holbertonschool.com>
2020-03-04 14:56:46 +0000

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,7 +15,6 @@ char *str_concat(char *s1, char *s2)
 
 	len1 = 0;
 	len2 = 0;
-	j = 0;
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 	while (s1[len1])
@@ -27,6 +26,7 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	for (i = 0; i < len1; i++)
 		s[i] = s1[i];
+	j = 0;
 	for (; i < len1 + len2; i++)
 	{
 		s[i] = s2[j];

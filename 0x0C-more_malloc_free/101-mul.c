46fe3ff5ea1879e4977844c81b80838b3f2fc4d8
Cirine92 <1410@holbertonschool.com>
2020-03-05 17:37:59 +0000

    101

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int ac, char **av)
+{
+	int num1 = 0, num2 = 0, mul;
+
+	if (ac != 3)
+	{
+		puts("Error");
+		exit(98);
+	}
+	else
+	{
+	num1 = atoi(av[1]);
+	num2 = atoi(av[2]);
+	mul = num1 * num2;
+	printf("%d\n", mul);
+	return (0);
+}
+}

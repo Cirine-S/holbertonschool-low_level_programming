d47d85146c08bbc1617159587909c73a38a94167
Cirine92 <1410@holbertonschool.com>
2020-03-05 14:12:48 +0000

    malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ *malloc_checked - function
+ *@b: int
+ *Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *m;
+	m = malloc(b);
+	if (m == NULL)
+		exit(98);
+	return (m);
+}

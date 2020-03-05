18e0bef709ebe8e141d3548e5b11dca6e0ff09be
Cirine92 <1410@holbertonschool.com>
2020-03-05 16:29:53 +0000

    100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *_realloc - function
+ *@ptr: void
+ *@old_size: int
+ *@new_size: int
+ *Return: void
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *r;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+		return (NULL);
+	r = malloc(new_size);
+	return (r);
+}

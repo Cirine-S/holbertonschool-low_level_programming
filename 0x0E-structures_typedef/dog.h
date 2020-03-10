f61fd0ff9a50675e81cf78119ba75c1e1e937720
Cirine92 <1410@holbertonschool.com>
2020-03-10 12:52:49 +0000

    3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,15 @@
  *@age: dog's age
  *@owner: dog's owner
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
+typedef struct dog dog_t;
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif

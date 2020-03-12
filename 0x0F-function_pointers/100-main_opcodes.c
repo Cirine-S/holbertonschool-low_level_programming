44b92636f2685db581e465abd9ad9f3458407c16
Cirine92 <1410@holbertonschool.com>
2020-03-12 08:51:40 +0000

    3

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,20 @@
+/**
+ *main - function
+ *@argc: int
+ *@argv: char
+ *Return: int
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		puts("Error");
+		exit(1);
+	}
+	if (argv[1] < 0)
+	{
+                puts("Error");
+                exit(2);
+        }
+	return(0);
+}

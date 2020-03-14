4dcbb5d9ae84dda0a57d475eff8cb78bb620be63
Cirine92 <1410@holbertonschool.com>
2020-03-14 13:53:49 +0000

    Variadic Functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,16 +10,16 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int sum = 0;
-	va_list list;
+	va_list ls;
 
 	if (n == 0)
 		return (0);
 
-	va_start(list, n);
+	va_start(ls, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(list, int);
+		sum += va_arg(ls, int);
 
-	va_end(list);
+	va_end(ls);
 	return (sum);
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,7 +11,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list list;
-	char * s;
+	char *s;
 
 	va_start(list, n);
 

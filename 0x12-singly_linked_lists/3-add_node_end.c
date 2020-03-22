fd3cd6d56f9266e23691aac7e6cac1b85a93ba90
Cirine92 <1410@holbertonschool.com>
2020-03-22 22:54:23 +0000

    add last node in the list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,30 +3,30 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- *add_node - function
+ *add_node_end - function
  *@head: list
  *@str: str
  *Return: list
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-        int i = 0;
-        list_t *node;
+	int i = 0;
+	list_t *node;
 	list_t *list;
 
-        if (!str)
+	if (!str)
 		return (NULL);
 
-        node = malloc(sizeof(list_t));
+	node = malloc(sizeof(list_t));
 
-        if (!node)
-                return (NULL);
+	if (!node)
+		return (NULL);
 
-        node->str = strdup(str);
-        for (i = 0; str[i]; i++)
-                ;
-        node->len = i;
-        node->next = NULL;
+	node->str = strdup(str);
+	for (i = 0; str[i]; i++)
+		;
+	node->len = i;
+	node->next = NULL;
 
 	if (*head == NULL)
 	*head = node;
@@ -39,7 +39,7 @@ list_t *add_node_end(list_t **head, const char *str)
 			list = list->next;
 
 	list->next = node;
-        }
+	}
 
 	return (node);
 }

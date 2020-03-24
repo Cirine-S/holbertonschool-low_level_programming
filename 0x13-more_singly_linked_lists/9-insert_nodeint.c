23a4e71ea30d12ef4e694a563d4f5b6b50d5c7cc
Cirine92 <1410@holbertonschool.com>
2020-03-24 23:11:34 +0000

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (newnode == NULL)
 		return (NULL);
 	newnode->n = n;
-        newnode->next = NULL;
+	newnode->next = NULL;
 
 	if (idx == 0)
 	{

f3d544c4adc0465013eeed0c16371d8ff49179ff
Cirine92 <1410@holbertonschool.com>
2020-03-24 21:31:46 +0000

    sum

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -10,7 +10,7 @@
  */
 int sum_listint(listint_t *head)
 {
-	int sum;
+	int sum = 0;
 
 	if (!head)
 		return (0);
@@ -19,6 +19,6 @@ int sum_listint(listint_t *head)
 	{
 		sum += head->n;
 		head = head->next;
-}
+	}
 	return (sum);
 }

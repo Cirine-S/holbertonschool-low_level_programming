feae5ee0fc1c903558c94adfabe1f2f899a1b3ac
Cirine92 <1410@holbertonschool.com>
2020-03-24 22:03:43 +0000

    insert node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *delete_nodeint_at_index - function
+ *@head: list
+ *@index: int
+ *Return: int
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+
+}

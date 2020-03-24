ddba4a22fe1a548a17772a21450d5f1499f87511
Cirine92 <1410@holbertonschool.com>
2020-03-24 23:07:23 +0000

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t *tmp = NULL, *node = *head;
+	listint_t *node = *head;
 	listint_t *newnode = NULL;
 
 	if ((*head) == NULL)
@@ -22,17 +22,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 		return (NULL);
+	newnode->n = n;
+        newnode->next = NULL;
 
-	for (i = 0; i <= index; i++)
+	if (idx == 0)
+	{
+		newnode->next = *head;
+		*head = newnode;
+	}
+
+	for (i = 0; i < idx - 1; i++)
 	{
 		if (!node->next)
 			return (NULL);
 		node = node->next;
 	}
-	tmp = node->next;
+	newnode->next = node->next;
 	node->next = newnode;
-	newnode->n = n;
-	newnode->next = tmp;
 
 	return (newnode);
 }

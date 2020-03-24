a983bd589bb36d5c69e36a4c1d97f8648a6ba65f
Cirine92 <1410@holbertonschool.com>
2020-03-24 22:21:21 +0000

    insert node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,5 +11,10 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	int i;
+	listint_t *node = (*head);
+
+	while (*head)
+	{
 
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,17 +13,17 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t *tmp = NULL;
+	listint_t *tmp = NULL, *node = *head;
 	listint_t *newnode = malloc(sizeof(listint_t));
 
 	for (i = 0; i < idx - 1; i++)
 	{
-		if (!(*head))
+		if (!node->next)
 			return (NULL);
-		*head = (*head)->next;
+		node = node->next;
 	}
-	tmp = (*head)->next;
-	(*head)->next = newnode;
+	tmp = node->next;
+	node->next = newnode;
 	newnode->n = n;
 	newnode->next = tmp;
 

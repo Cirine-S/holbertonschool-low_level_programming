e4d8cf3314ce9a18551436122f9847c6f817ef44
Cirine92 <1410@holbertonschool.com>
2020-03-26 22:05:11 +0000

    11

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ *set_bit - function
+ *@n: int
+ *@index: int
+ *Return: int
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	*n = *n || (1 << index);
+	return (1);
+
+}

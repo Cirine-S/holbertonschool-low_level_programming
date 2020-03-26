f63784679c4f13a883ba553a4d89d41a77579bfb
Cirine92 <1410@holbertonschool.com>
2020-03-26 20:33:41 +0000

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,12 +5,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
-		_putchar('0');
-	else
-	while (n)
-	{
-		_putchar((n & 1) + '0');
-		n = (n >> 1);
-	}
+	if (n > 1)
+	print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }

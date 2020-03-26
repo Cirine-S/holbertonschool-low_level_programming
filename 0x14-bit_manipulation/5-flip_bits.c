52b967701e3db6b5b5f490a3105724c879f1663b
Cirine92 <1410@holbertonschool.com>
2020-03-26 22:44:19 +0000

    101

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *flip_bits
+ *@n: int
+ *@m: int
+ *Return: unsigned int
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int flip = 0, i = 0;
+
+	flip = n ^ m;
+	while (flip)
+	{
+		i+= flip & 1;
+		flip >>= 1;
+	}
+	return (i);
+}

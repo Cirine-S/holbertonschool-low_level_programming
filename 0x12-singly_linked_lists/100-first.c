57249c4e8f8c234f3111d40f143c12f69d9781fd
Cirine92 <1410@holbertonschool.com>
2020-03-27 14:17:44 +0000

    before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <stdio.h>
+/**
+ *printbeforemain - function
+ */
+void printbeforemain(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon ");
+	printf("my back!\n");
+}

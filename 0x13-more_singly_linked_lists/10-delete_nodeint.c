6b1badfed97a37db5da1711f1b404b20c1557062
Cirine92 <1410@holbertonschool.com>
2020-03-29 16:00:15 +0000

    delete extra *

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,9 +14,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *tmp = NULL, *node = *head;
 
-	if ((*head) == NULL)
-		return (-1);
-
 	if (index == 0)
 	{
 		tmp = (*head)->next;

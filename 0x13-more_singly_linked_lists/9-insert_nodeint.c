edf21aa3944e330e9e6526bf64f0e75172797b7d
Cirine92 <1410@holbertonschool.com>
2020-03-29 15:33:06 +0000

    return case 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,6 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		newnode->next = *head;
 		*head = newnode;
+		return (newnode);
 	}
 
 	for (i = 0; i < idx - 1; i++)

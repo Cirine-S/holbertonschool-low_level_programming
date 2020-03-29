aa83cbc1bdec471045102c931c6dc051274ad3d5
Cirine92 <1410@holbertonschool.com>
2020-03-29 15:58:52 +0000

    delete extra *

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,9 +16,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *node = *head;
 	listint_t *newnode = NULL;
 
-	if (head == NULL)
-	return (NULL);
-
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 		return (NULL);

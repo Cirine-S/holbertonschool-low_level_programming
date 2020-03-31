9c2a19ff10517bf531ecb5b7698c42abf68b399b
Cirine92 <1410@holbertonschool.com>
2020-03-31 20:01:42 +0000

    2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * append_text_to_file - function
+ * @filename: char
+ * @text_content: char*
+ * Return: int
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, nb, i;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		return (1);
+
+	for (i = 0; text_content[i]; i++)
+		;
+
+	nb = write(fd, text_content, i);
+	if (nb == -1)
+		return (-1);
+
+	close(fd);
+	
+}

89c9c9e2048bd5388aba8daf9443dbad46188d64
Cirine92 <1410@holbertonschool.com>
2020-05-03 16:16:59 +0000

    listint

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ *listint_len - function
+ *@h: list
+ *Return: size
+ */
+size_t listint_len(const listint_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}

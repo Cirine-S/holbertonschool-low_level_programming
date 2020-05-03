be218046957b3bfd15a06c5e5ba86afa82336a1b
Cirine92 <1410@holbertonschool.com>
2020-05-03 21:07:35 +0000

    delete @index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -20,14 +20,13 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = tmp->next;
-		(*head)->prev = NULL;
 		free(tmp);
 		return (1);
 	}
 
 	for (i = 0; i < index && tmp->next != NULL; i++)
 	{
-		node =tmp;
+		node = tmp;
 		tmp = tmp->next;
 	}
 

013e0b988886393eedaaf483faa40a9f9c9c8d5e
Cirine92 <1410@holbertonschool.com>
2020-05-04 15:09:25 +0000

    case: add element in empty list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -36,20 +36,19 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	for (i = 0; i < idx - 1; i++)
 	{
+		if (!node->next)
+			return (NULL);
 		node = node->next;
 	}
-	if (node->next)
-	{
 	storenext = node->next;
 	node->next = newnode;
 	newnode->prev = node;
+
+	if (node->next)
+	{
 	newnode->next = storenext;
 	storenext->prev = newnode;
 	}
-	else
-	{
-		newnode->prev = node;
-		node->next = newnode;
-	}
+
 	return (newnode);
 }

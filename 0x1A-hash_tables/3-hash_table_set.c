f32dc9ca4887005b3c5145162d4d0fcf867e4a1f
Cirine92 <1410@holbertonschool.com>
2020-05-29 20:50:08 +0000

    add node

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *hn;
 	hash_node_t *tmp;
-	int i = 0;
+	unsigned long int i = 0;
 
 	if (ht == NULL || key == NULL)
 		return (0);

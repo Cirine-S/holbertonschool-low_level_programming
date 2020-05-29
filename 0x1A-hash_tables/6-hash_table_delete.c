7449ce81d9e4f6629b479644cb3e4b3ca98bbddc
Cirine92 <1410@holbertonschool.com>
2020-05-29 22:53:24 +0000

    correct unsigned

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -23,7 +23,7 @@ void hash_table_delete(hash_table_t *ht)
 		{
 			n = tmp->next;
 			free(tmp->key);
-                        free(tmp->value);
+			free(tmp->value);
 			free(tmp);
 			tmp = n;
 		}

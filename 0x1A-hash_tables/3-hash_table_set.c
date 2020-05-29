2f0dc86c9e1775a85ce622d26cf92ce083363c0b
Cirine92 <1410@holbertonschool.com>
2020-05-29 21:10:36 +0000

    correct cast

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || key == NULL)
 		return (0);
 
-	i = key_index((const unsigned char *)key, ht->size);
+	i = key_index((void *)key, ht->size);
 	tmp = ht->array[i];
 
 
@@ -41,7 +41,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (!hn)
 			return (0);
 
-		hn->key = (void *)key;
+		hn->key = strdup(key);
 		hn->value = strdup(value);
 		hn->next = ht->array[i];
 		ht->array[i] = hn;

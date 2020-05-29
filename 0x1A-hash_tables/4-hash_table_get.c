336af214558661d0ff0a1ab14e5fae4e6fb78225
Cirine92 <1410@holbertonschool.com>
2020-05-29 22:00:26 +0000

    get value

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get a value assiociated with a key
+ *@ht: hash table
+ *@key: key to find
+ * Return: value
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	int i = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *tmp;
+
+	tmp = ht->array[i];
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}

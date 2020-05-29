604bc88de742003cf4c5e43c6322bcc89fe43935
Cirine92 <1410@holbertonschool.com>
2020-05-29 20:48:17 +0000

    add node

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || key == NULL)
 		return (0);
 
-	i = key_index((void *)key, ht->size);
+	i = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[i];
 
 

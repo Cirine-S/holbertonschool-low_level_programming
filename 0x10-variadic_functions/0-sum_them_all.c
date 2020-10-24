44b3bc88f18e445655c7bf0eec788a4e8a276ec4
cirine <1410@holbertonschool.com>
2020-10-24 17:10:32 +0100

    h

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,25 +1,24 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "variadic_functions.h"
+
 /**
- *sum_them_all - function
- *@n: int
- *Return: int
+ *sum_them_all - function that returns the sum of all its parameters
+ *@n: const unsigned int whish is the positif number of arguments
+ *Return: sum of all parameters
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i;
 	int sum = 0;
-	va_list list;
+	va_list ls;
+	unsigned int x;
 
 	if (n == 0)
 		return (0);
 
-	va_start(list, n);
-
-	for (i = 0; i < n; i++)
-		sum += va_arg(list, int);
-
-	va_end(list);
+	va_start(ls, n);
+	for (x = 0; x < n; x++)
+	{
+		sum = va_arg(ls, int) + sum;
+	}
+	va_end(ls);
 	return (sum);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,20 +1,24 @@
-#ifndef variadic
-#define variadic
+#ifndef _VARIADIC_FUNCTIONS_
+#define _VARIADIC_FUNCTIONS_
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
+
 /**
- * struct ch - Struct
- *
- * @c: c
- * @f: The function associated
+ * struct ch - struct
+ *@c: char
+ *@f: function associated
  */
 typedef struct ch
 {
 	char c;
 	void (*f)(va_list);
-} ch_p;
+}ch_f;
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
 #endif
